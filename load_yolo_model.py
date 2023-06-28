f01d1c54fc16efb14cb27a1d214e4c7e2c921c9f
williebees <66076410+williebees@users.noreply.github.com>
2023-06-28 10:59:42 -0400

    Replaced opencv's videowriter entirely due to instability, now writing a video should work.

--- load_yolo_model.py
+++ load_yolo_model.py
@@ -5,9 +5,10 @@ import cv2
 import os
 import numpy as np
 import ultralytics
-import ffmpeg
-from loguru import logger
 import os
+from skvideo.io import FFmpegWriter
+from tqdm import tqdm
+
 
 def load_yolo_model(model_path):
     model = torch.load(model_path)
@@ -40,16 +41,15 @@ def load_model_ultralytics(model_path):
     return model
 
 
-def run_predictions_on_video(model, video_filepath, show: Optional[bool] = False):
+def run_predictions_on_video(model, video_filepath, show: Optional[bool] = False, max_frames: Optional[int] = None):
     capture = cv2.VideoCapture(video_filepath)
-    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    # fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
     count = 0
     frames = []
     while True:
         ret, frame = capture.read()
         if frame is None:
-            #logger.warning("Frame is not currently available.")
             break
         predictions = model.predict(frame)
         results = predictions[0]
@@ -60,37 +60,27 @@ def run_predictions_on_video(model, video_filepath, show: Optional[bool] = False
             color = (140, 230, 240) if class_id == 1 else (0, 0, 255)
             cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
 
-        #frame_filename = f"videoframes/frame%{count}d.jpg"
-        #cv2.imwrite(frame_filename, frame)
-        #count += 1
-
-        if count == 60:
-            break
-
+        frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         count += 1
-
-        frames.append(frame)
-
+        if max_frames is not None and count >= max_frames:
+            break
         if show:
             cv2.imshow('frame', frame)
             if cv2.waitKey(30) & 0xFF == ord('q'):
                 break
-        #else:
-            #video_writer.write(frame)
-
-    video_writer = cv2.VideoWriter('output7.mp4', fourcc, 30, (480, 640))
-    for frame in frames:
-        video_writer.write(frame)
-
     capture.release()
-    video_writer.release()
+    print(f'writing video with {len(frames)} frames...')
+    video_writer = FFmpegWriter(filename='output7.mp4')
+    for frame in tqdm(frames):
+        video_writer.writeFrame(frame)
+    video_writer.close()
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     model_path = os.path.abspath('./runs/detect/train7/weights/best.pt')
     video_filepath = os.path.abspath('videos/AppMAIS3LB@2023-06-26@11-55-00.h264')
     frame_ind = 120
     model = load_model_ultralytics(model_path)
-    run_predictions_on_video(model, video_filepath, show=False)
-    #os.system("ffmpeg -f image2 -r 1/0.3 -i ./videoframes/* -vcodec mpeg4 -y ./videos/3LB_2023-06-26@11-55-00_output.mp4")
+    run_predictions_on_video(model, video_filepath, show=False, max_frames=60)
 
