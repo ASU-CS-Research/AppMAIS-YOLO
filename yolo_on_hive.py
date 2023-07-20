2ca2516d95d192b5223db3b6bf8cdc3fa48c368c
Tayo <tolofi152@gmail.com>
2023-07-20 13:41:28 -0400

    made yolo_on_hive.py into functions

--- yolo_on_hive.py
+++ yolo_on_hive.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 
 # metrics_map = ""
 # label_list = os.listdir("./videos")
-# hives = []
+hives = ['2RB', '2L', '5R', '11R', '16LB', '12L', '4LB', '6RC', '10LB', '10RB', '16R', '8LB', '7R', '1L', '1R', '9R', '3RB', '3LB', '5LB', '12R', '8R', '13R', '13L', '14L', '14R', '7L', '9LB', '6L', "11L"]
 
 # for name in label_list:
 #     if name == "classes.txt" or len(name) < 5:
@@ -30,24 +30,28 @@ from sklearn.model_selection import train_test_split
 
 #print(hives)
 
-hives = ['2RB', '2L', '5R', '11R', '16LB', '12L', '4LB', '6RC', '10LB', '10RB', '16R', '8LB', '7R', '1L', '1R', '9R', '3RB', '3LB', '5LB', '12R', '8R', '13R', '13L', '14L', '14R', '7L', '9LB', '6L', "11L"]
 
 #sorting, will mess up for 2 digit hives
-# labels = os.listdir("/home/bee/bee-detection/data_appmais_lab/labels")
-# dest_path = "/home/bee/bee-detection/data_appmais_lab/data_by_hive/"
-# src_path = "/home/bee/bee-detection/data_appmais_lab/"
-#
-# for label in labels:
-#     for hive in hives:
-#         if hive in label:
-#             file_name = label[:-4]
-#
-#             # put the labeled file in the respective hive folder
-#             shutil.copyfile(f"{src_path}labels/{label}", f"{dest_path}{hive}/labels/{label}")
-#
-#             # put the image file in the respective hive folder
-#             shutil.copyfile(f"{src_path}images/{file_name}.png", f"{dest_path}{hive}/images/{file_name}.png")
-#             # print(file_name)
+def sorting_by_hive( src, dest):
+    # dest_path = "/home/bee/bee-detection/data_appmais_lab/data_by_hive/"
+    # src_path = "/home/bee/bee-detection/data_appmais_lab/"
+
+    src_path = src
+    dest_path = dest
+    labels = os.listdir(f"{src_path}labels")
+
+
+    for label in labels:
+        for hive in hives:
+            if hive in label and ("1" + hive not in label):
+                file_name = label[:-4]
+
+                # put the labeled file in the respective hive folder
+                shutil.copyfile(f"{src_path}labels/{label}", f"{dest_path}{hive}/labels/{label}")
+
+                # put the image file in the respective hive folder
+                shutil.copyfile(f"{src_path}images/{file_name}.png", f"{dest_path}{hive}/images/{file_name}.png")
+    #             # print(file_name)
 
 #correcting sorting mistakes
 # for hive in ["1L", "1R", "2L"]:
@@ -67,39 +71,52 @@ hives = ['2RB', '2L', '5R', '11R', '16LB', '12L', '4LB', '6RC', '10LB', '10RB', 
 #                         # print(file_name)
 
 # making the folders
-# for hive in hives:
-#     data = "/home/bee/bee-detection/data_appmais_lab/data_by_hive"
-#     os.mkdir(f"{data}/{hive}")
-#     os.mkdir(f"{data}/{hive}/images/")
-#     os.mkdir(f"{data}/{hive}/labels/")
+def format_hives_directory(dest):
+    for hive in hives:
+        # data = f"/home/bee/bee-detection/data_appmais_lab/data_by_hive"
+        data = f"{dest}/data_by_hive"
+        os.mkdir(f"{data}/{hive}")
+        os.mkdir(f"{data}/{hive}/images/")
+        os.mkdir(f"{data}/{hive}/labels/")
 
 #writing the yaml files
-# for hive in hives:
-#     data_path = f"{dest_path}{hive}/"
-#     yaml = f"train: /home/bee/bee-detection/data/train/images\nval: {data_path}\ntest: /home/bee/bee-detection/data/test/images\n\nnc: 2\nnames: ['Drone', 'Worker']"
-#     output_filepath = f"{dest_path}{hive}/{hive}data.yaml"
-#     yaml_file = open(output_filepath, "w")
-#     # print(output_filepath)
-#     yaml_file.write(yaml)
-#     yaml_file.close()
+def write_yamls(dest_path, val_path):
+    for hive in hives:
+        data_path = f"{dest_path}{hive}/"
+        yaml = f"train: /home/bee/bee-detection/data/train/images\nval: {val_path}\ntest: /home/bee/bee-detection/data/test/images\n\nnc: 2\nnames: ['Drone', 'Worker']"
+        output_filepath = f"{dest_path}{hive}/{hive}data.yaml"
+        yaml_file = open(output_filepath, "w")
+        # print(output_filepath)
+        yaml_file.write(yaml)
+        yaml_file.close()
 
 # print(label_list)
 
 
 #running yolo on a video/hive data and gettimg the metrics
-# pretrained_weights = os.path.abspath("/home/olofintuyita/AppMAIS-YOLO/runs/detect/train7/weights/best.pt")
-# model = ultralytics.YOLO(model=pretrained_weights)
-# # model.train(data='data.yaml', epochs=2, imgsz=(480, 640), verbose=True, batch=8, lr0=0.001)
-#
-# hive_metrics = {}
-#
-# for hive in ["1L", "1R", "2L"]:
-#     data_yaml = os.path.abspath(f"/home/olofintuyita/AppMAIS-YOLO/data_by_hive/{hive}/{hive}data.yaml")
-#     metrics = model.val(data_yaml)
-#     hive_metrics[hive] = metrics
-#     model.export()
-#
-#     #lym.run_predictions_on_video(model=model, video_filepath="/home/olofintuyita/AppMAIS-YOLO/videos/AppMAIS7L@2023-06-26@11-55-00.h264", destination_video_path=f"output{hive}.mp4", show=False)
-#
-# for key in hive_metrics.keys():
-#     print(f"\n\n{key}\n\n{hive_metrics[key]}\n\n")
+def metrics_on_hives(path, val_hives = hives):
+    pretrained_weights = os.path.abspath("/home/olofintuyita/AppMAIS-YOLO/runs/detect/train7/weights/best.pt")
+    model = ultralytics.YOLO(model=pretrained_weights)
+    # model.train(data='data.yaml', epochs=2, imgsz=(480, 640), verbose=True, batch=8, lr0=0.001)
+
+    hive_metrics = {}
+
+    for hive in val_hives:
+        if not path:
+            path = f"/home/olofintuyita/AppMAIS-YOLO/data_by_hive/{hive}/{hive}data.yaml"
+            data_yaml = os.path.abspath(path)
+        else:
+            data_yaml = os.path.abspath(f"{path}/{hive}/{hive}data.yaml")
+        metrics = model.val(data_yaml)
+        hive_metrics[hive] = metrics
+        model.export()
+
+    for key in hive_metrics.keys():
+        print(f"\n\n{key}\n\n{hive_metrics[key]}\n\n")
+
+
+
+#lym.run_predictions_on_video(model=model, video_filepath="/home/olofintuyita/AppMAIS-YOLO/videos/AppMAIS7L@2023-06-26@11-55-00.h264", destination_video_path=f"output{hive}.mp4", show=False)
+
+if __name__ == '__main__':
+    _ = ""
