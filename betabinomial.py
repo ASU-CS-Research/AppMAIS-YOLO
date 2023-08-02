8bbe1763a467b02a167dc80c7950b1e18bd2a930
williebees <66076410+williebees@users.noreply.github.com>
2023-08-02 14:03:24 -0400

    Fixed an issue where the labels did not match up to the actual images.

--- betabinomial.py
+++ betabinomial.py
@@ -1,13 +1,39 @@
 import matplotlib.pyplot as plt
 from scipy.stats import betabinom, binom, beta
 import numpy as np
-from typing import List
+from typing import List, Optional
 import ultralytics
 import os
 import cv2 as cv
 
-def beta_binom_on_data(model, images: List[np.ndarray], labels: np.ndarray, image_filenames: List[str]) -> List[float]:
-    results = model(images)
+
+def get_model_results(model, images, labels=None, image_filenames=None, copy_images_and_labels=False, output_dir=None):
+    results = model.predict(images)
+    # if copy_images_and_labels and output_dir is not None:
+    #     images_for_model_output = [np.copy(image) for image in images]
+    #     images_for_label_output = [np.copy(image) for image in images]
+    #     # Apply bounding boxes to images
+    #     for model_image, label_image, image_filename, result, label in (
+    #             zip(images_for_model_output, images_for_label_output, image_filenames, results, labels)):
+    #         # Plot the model output
+    #         bounding_boxes = result.boxes
+    #         for box in bounding_boxes:
+    #             x1, y1, x2, y2, conf, class_id = box.data.tolist()[0]
+    #             cv.rectangle(model_image, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
+    #             cv.putText(model_image, str(conf), (int(x1), int(y1)), cv.FONT_HERSHEY_SIMPLEX, 1,
+    #                        (255, 0, 0), 2)
+    #         # Plot the human made labels
+    #         for label in labels:
+    #             # Label is given as
+    #         # save the images
+    #         model_output_path = os.path.join(output_dir, 'model_outputs', image_filename.replace('.png', '') + "_output.png")
+    #         cv.imwrite(model_output_path, model_image)
+    return results
+
+
+def beta_binom_on_data(model, images: List[np.ndarray], labels: np.ndarray, image_filenames: List[str],
+                       copy_images_and_labels: Optional[bool] = False) -> List[float]:
+    results = get_model_results(model, images, copy_images_and_labels)
     log_likelyhoods = []
 
     for result, label, filename in zip(results, labels, image_filenames):
@@ -94,18 +120,17 @@ def beta_binom_on_data(model, images: List[np.ndarray], labels: np.ndarray, imag
 if __name__ == "__main__":
     path = "/home/bee/bee-detection/data_appmais_lab/AppMAIS11s_labeled_data/split_dataset/val/"
 
-    labels_filenames = os.listdir(f"{path}labels/")
-    labels = []
-
     model_11s = ultralytics.YOLO("/home/bee/bee-detection/trained_on_11s.pt")
     # model_1s = ultralytics.YOLO("/home/bee/bee-detection/trained_on_1s.pt")
 
     images_filenames = os.listdir(f"{path}images/")
     images = [cv.imread(os.path.join(path,"images",image_path)) for image_path in images_filenames]
-
+    labels_filenames = [os.path.join(path,"labels",image_path.replace(".png",".txt"))
+                        for image_path in images_filenames]
+    labels = []
 
     for label_filename in labels_filenames:
-        with open(f"{path}labels/{label_filename}", "r") as f:
+        with open(label_filename, "r") as f:
             lines = f.readlines()
             image_label = []
             for line in lines:
