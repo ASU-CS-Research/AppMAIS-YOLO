d064f94d3d70a78c9544075b03c04292b50368d3
williebees <66076410+williebees@users.noreply.github.com>
2023-12-05 11:27:14 -0500

    Made the single frame retrieval process in load_yolo_model.py a little simpler

--- load_yolo_model.py
+++ load_yolo_model.py
@@ -42,6 +42,17 @@ def load_model_ultralytics(model_path):
     return model
 
 
+def draw_boxes_on_frame(frame, results):
+    bounding_boxes = results.boxes
+    for box in bounding_boxes:
+        x1, y1, x2, y2, conf, class_id = box.data.tolist()[0]
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        colors = ((0, 180, 255), (255, 180, 0))
+        color = colors[int(class_id)]
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+    return frame
+
+
 def run_predictions_on_video(model, video_filepath, destination_video_path, show: Optional[bool] = False,
                              max_frames: Optional[int] = None, batch_size: Optional[int] = 16,
                              conf: Optional[float] = 0.2):
@@ -60,14 +71,7 @@ def run_predictions_on_video(model, video_filepath, destination_video_path, show
     for batch in tqdm(batches):
         predictions = model.predict(list(frames[batch]), conf=conf)
         for frame, results in zip(frames[batch], predictions):
-            bounding_boxes = results.boxes
-            for box in bounding_boxes:
-                x1, y1, x2, y2, conf, class_id = box.data.tolist()[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                color = (140, 230, 240) if class_id == 1 else (0, 0, 255)
-                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-                # Write the confidence on the image
-                cv2.putText(frame, f'{conf:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+            frame = draw_boxes_on_frame(frame, results)
             edited_frames.append(frame)
             count += 1
             if max_frames is not None and count >= max_frames:
@@ -107,11 +111,24 @@ def run_predictions_on_video(model, video_filepath, destination_video_path, show
 
 
 if __name__ == '__main__':
-    model_path = os.path.abspath('./runs/detect/train7/weights/best.pt')
-    video_filepath = os.path.abspath('videos/AppMAIS3LB@2023-06-26@11-55-00.h264')
-    frame_ind = 120
+    model_path = os.path.abspath('/home/bee/bee-detection/final_model.pt')
+    video_filepath = os.path.abspath('videos/AppMAIS11R@2022-09-13@13-40-00.h264')
+    frame_ind = 248
     model = load_model_ultralytics(model_path)
-    destination_video_path = os.path.abspath('output21.mp4')
-    run_predictions_on_video(model = model, video_filepath = video_filepath, destination_video_path = destination_video_path, show=False)
+    # destination_video_path = os.path.abspath('output21.mp4')
+    # run_predictions_on_video(model = model, video_filepath = video_filepath, destination_video_path = destination_video_path, show=False)
+    desired_frame = get_frame_from_video(video_filepath, frame_ind)
+    desired_frame_2 = desired_frame.copy()
+    desired_frame_64 = desired_frame.copy()
+    desired_frame_8 = desired_frame.copy()
+    predictions2 = model.predict(desired_frame_2, conf=0.2)
+    predictions64 = model.predict(desired_frame_64, conf=0.64)
+    predictions8 = model.predict(desired_frame_8, conf=0.8)
+    conf2frame = draw_boxes_on_frame(desired_frame_2, predictions2[0])
+    conf64frame = draw_boxes_on_frame(desired_frame_64, predictions64[0])
+    conf8frame = draw_boxes_on_frame(desired_frame_8, predictions8[0])
+    # Save the frames
+    cv2.imwrite('conf2frame.png', conf2frame)
+    cv2.imwrite('conf64frame.png', conf64frame)
+    cv2.imwrite('conf8frame.png', conf8frame)
 
-    # print(model.info())
