0aa0ee9d93b99c7c5a162e6bedcf8a9a53ff30e7
TayoOlofintuyi <tolofi152@gmail.com>
2024-11-14 14:57:48 -0500

    trying to align the boxes from the predictions to the labels

--- roc_curve.py
+++ roc_curve.py
@@ -8,7 +8,6 @@ import cv2 as cv
 from betabinomial import beta_binom_on_data, parse_images_and_labels, rmse_on_data, get_caste_count_labels_results, \
     get_model_results
 
-
 # data_path = os.path.abspath("/home/bee/bee-detection/data_appmais_lab/AppMAIS1s_labeled_data/val/")
 # data_path = os.path.abspath('/home/bee/bee-detection/data_appmais_lab/AppMAIS11s_labeled_data/final_split_dataset/test')
 # data_path = os.path.abspath('/home/bee/bee-detection/data_appmais_lab/AppMAIS1s_labeled_data/complete_data')
@@ -21,4 +20,28 @@ ultralytics_model = ultralytics.YOLO(model_path)
 images, images_filenames, labels_list = parse_images_and_labels(data_path)
 
 output_directory = os.path.abspath('/home/bee/bee-detection/model_and_label_outputs/')
+
+results, _, _ = get_model_results(ultralytics_model, images, labels_list, images_filenames)
+
+print(results)
+# exit(1)
+
+# print(f"result 1 boxes: \n{results[0][0].boxes[0][0]}")
+#
+# print(f"label 1: \n{labels_list[0][0]}")
+
+
+boxes = results[0].boxes
+print(f"Box object:")
+print(f"box: {boxes.cls}")
+print(f"box: {boxes.xyxy}")
+
+predictions = []
+
+for img, cls, xyxy in zip(images, boxes.cls, boxes.xyxy):
+    predictions.append({ "class": cls, "xyxy": xyxy})
+
+print(predictions)
+
+print(f"result[0].boxes: {results[0].boxes[0].data.tolist()[0]}")
 
