b368bd0531082bcb489aea31a1fde0bef8fbc093
TayoOlofintuyi <tolofi152@gmail.com>
2024-11-21 14:58:02 -0500

    trying to resolve error: for box in image_results.boxes[0][1]: IndexError: index 1 is out of bounds for dimension 0 with size 1

--- roc_curve.py
+++ roc_curve.py
@@ -158,8 +158,37 @@ def get_tpr_fpr(label_pred_counts: List[List[int]]):
     return drone_tpr, drone_fpr, worker_tpr, worker_fpr
 
 def get_tpr_fpr_by_iou_by_image(image_labels, image_results):
+    """
+    Getting the true positive rate and false positive rate for each image.
+    true positives are boxes that have an iou > 0.5 with a label and have the same class as the label.
+    false positives are boxes that have an iou < 0.5 with a label or have a different class than the label.
+    from : https://medium.com/lunit/evaluation-curves-for-object-detection-algorithms-in-medical-images-4b083fddce6e
+
+    :param image_labels:
+    :param image_results:
+    :return:
+    """
     boxes_xyxy = [image_result.boxes[0][0].data.tolist()[0] for image_result in image_results]
-    boxes_cls = [image_result.boxes[0][1].data.tolist()[0] for image_result in image_results]
+
+    if len(boxes_xyxy) == 0 or len(image_labels) == 0 or len(image_results.boxes) == 0:
+        return 0, 0
+
+    # boxes_cls = [image_result.boxes[0][1].data.tolist()[0] for image_result in image_results]
+
+    # boxes_cls = [box.cls for box in image_results.boxes[0][1].data.tolist()[0]]
+
+    boxes_cls = []
+    for box in image_results.boxes[0][1]:
+        box_data_list = box.data.tolist()[0]
+        cls = box_data_list.cls
+        boxes_cls.append(cls)
+
+    # boxes_cls = []
+    # for image_result in image_results:
+    #     box = image_result.boxes[0][1]
+    #     cls = box.data.tolist()[0]
+    #     boxes_cls.append(cls)
+
     boxes = [[cls, *xyxy] for cls, xyxy in zip(boxes_cls, boxes_xyxy)]
 
     # boxes = list of elements like [cls, x1, y1, x2, y2], predicted by the model
@@ -168,10 +197,15 @@ def get_tpr_fpr_by_iou_by_image(image_labels, image_results):
     tp = 0
     fp = 0
     samples = 0
+
+    # 2 predicted boxes that have the same class and an iou > 0.5 with the same label should not be counted as
+    # 2 true positives. Therefor, we keep track of the labels that have been accurately matched to a predicted box.
     predicted_labels = [0 for _ in range(len(image_labels))]
 
     for box in boxes:
         samples += 1
+
+        # figure out which label the box has the highest iou with
         iou_scores = [iou(box[1:], label[1:]) for label in image_labels]
         idx_max_iou = np.argmax(iou_scores)
 
@@ -179,6 +213,7 @@ def get_tpr_fpr_by_iou_by_image(image_labels, image_results):
             pass
 
         try:
+            # if the closest label has already been matched to a box, find the next closest label
             while predicted_labels[idx_max_iou] == 1:
                 iou_scores[idx_max_iou] = 0
                 try:
@@ -191,24 +226,22 @@ def get_tpr_fpr_by_iou_by_image(image_labels, image_results):
         except OutOfLabels:
             continue
 
+        # if the iou is less than 0.5, the box is not close enough to any label and is a false positive
+        # if the box has a different class than the label it is closest to, it is a false positive
         if iou_scores[idx_max_iou] < 0.5 or box[0] != image_labels[idx_max_iou][0]:
             fp += 1
         else:
+            # otherwise, the box is a true positive. Also, mark the label as matched to a box
             predicted_labels[idx_max_iou] = 1
             tp += 1
 
+    if samples == 0:
+        return 0, 0
 
+    tpr = tp / samples
+    fpr = fp / samples
+    return tpr, fpr
 
-
-        # if iou_scores[idx_max_iou] > 0.5:
-        #     if predicted_labels[idx_max_iou] == 0:
-        #     if box[0] == 0:
-        #         tp += 1
-        #     else:
-        #         fp += 1
-        # else:
-        #     if box[0] == 0:
-        #         fp += 1
 
 def area_under_curve(tpr: List[float], fpr: List[float]) -> float:
     auc = 0
@@ -216,46 +249,82 @@ def area_under_curve(tpr: List[float], fpr: List[float]) -> float:
         auc += (fpr[i] - fpr[i - 1]) * (tpr[i] + tpr[i - 1]) / 2
     return auc
 
-conf_and_rates = []
+def drone_worker_roc_curve(): # deprecated
+    conf_and_rates = []
 
-confs = np.arange(0.0, 1.0, 0.05)
-# reverse confs
-confs = confs[::-1]
-for conf in confs:
-    label_pred_counts = get_label_pred_counts_for_conf(conf)
-    drone_tpr, drone_fpr, worker_tpr, worker_fpr = get_tpr_fpr(label_pred_counts)
-    print(f"conf: {conf} drone_tpr: {drone_tpr} drone_fpr: {drone_fpr} worker_tpr: {worker_tpr} worker_fpr: {worker_fpr}")
-    conf_and_rates.append([conf, drone_tpr, drone_fpr, worker_tpr, worker_fpr])
+    confs = np.arange(0.0, 1.0, 0.05)
+    # reverse confs
+    confs = confs[::-1]
+    for conf in confs:
+        label_pred_counts = get_label_pred_counts_for_conf(conf)
+        drone_tpr, drone_fpr, worker_tpr, worker_fpr = get_tpr_fpr(label_pred_counts)
+        print(f"conf: {conf} drone_tpr: {drone_tpr} drone_fpr: {drone_fpr} worker_tpr: {worker_tpr} worker_fpr: {worker_fpr}")
+        conf_and_rates.append([conf, drone_tpr, drone_fpr, worker_tpr, worker_fpr])
 
-pprint(conf_and_rates)
+    pprint(conf_and_rates)
 
-conf_and_rates = np.array(conf_and_rates)
+    conf_and_rates = np.array(conf_and_rates)
 
-plt.plot(conf_and_rates[:, 2], conf_and_rates[:, 1], label="drone")
-# annotate the conf for each point
-for conf, drone_tpr, drone_fpr, worker_tpr, worker_fpr in conf_and_rates:
-    plt.annotate(f"{conf:.2f}", (drone_fpr, drone_tpr))
-plt.xlabel("FPR")
-plt.ylabel("TPR")
-plt.title("Drone ROC curve")
-plt.show()
+    plt.plot(conf_and_rates[:, 2], conf_and_rates[:, 1], label="drone")
+    # annotate the conf for each point
+    for conf, drone_tpr, drone_fpr, worker_tpr, worker_fpr in conf_and_rates:
+        plt.annotate(f"{conf:.2f}", (drone_fpr, drone_tpr))
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.title("Drone ROC curve")
+    plt.show()
 
-plt.plot(conf_and_rates[:, 4], conf_and_rates[:, 3], label="worker")
-# annotate the conf for each point
-for conf, drone_tpr, drone_fpr, worker_tpr, worker_fpr in conf_and_rates:
-    plt.annotate(f"{conf:.2f}", (worker_fpr, worker_tpr))
-plt.xlabel("FPR")
-plt.ylabel("TPR")
-plt.title("Worker ROC curve")
+    plt.plot(conf_and_rates[:, 4], conf_and_rates[:, 3], label="worker")
+    # annotate the conf for each point
+    for conf, drone_tpr, drone_fpr, worker_tpr, worker_fpr in conf_and_rates:
+        plt.annotate(f"{conf:.2f}", (worker_fpr, worker_tpr))
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.title("Worker ROC curve")
 
-auc_drone = area_under_curve(conf_and_rates[:, 1], conf_and_rates[:, 2])
-auc_worker = area_under_curve(conf_and_rates[:, 3], conf_and_rates[:, 4])
+    auc_drone = area_under_curve(conf_and_rates[:, 1], conf_and_rates[:, 2])
+    auc_worker = area_under_curve(conf_and_rates[:, 3], conf_and_rates[:, 4])
 
-plt.show()
+    plt.show()
 
-print(f"Drone AUC: {auc_drone}")
-print(f"Worker AUC: {auc_worker}")
-#  why is the output of the area under curve function negative?
-#  the output of the area under curve function is negative because the fpr and tpr values are not sorted in ascending order.
-print(f"conf_and_rates is conf, drone_tpr, drone_fpr, worker_tpr, worker_fpr")
-pprint(conf_and_rates)
+    print(f"Drone AUC: {auc_drone}")
+    print(f"Worker AUC: {auc_worker}")
+    #  why is the output of the area under curve function negative?
+    #  the output of the area under curve function is negative because the fpr and tpr values are not sorted in ascending order.
+    print(f"conf_and_rates is conf, drone_tpr, drone_fpr, worker_tpr, worker_fpr")
+    pprint(conf_and_rates)
+
+
+def roc_curve_by_iou():
+
+    conf_and_rates = []
+
+    confs = np.arange(0.0, 1.0, 0.05)
+
+    # reverse confs
+    confs = confs[::-1]
+
+    for conf in confs:
+        results = ultralytics_model.predict(images, conf=conf)
+
+        for labels, result in zip(labels_list, results):
+            tpr, fpr = get_tpr_fpr_by_iou_by_image(labels, result)
+            print(f"conf: {conf} tpr: {tpr} fpr: {fpr}")
+            conf_and_rates.append([conf, tpr, fpr])
+
+    plt.plot(conf_and_rates[:, 2], conf_and_rates[:, 1])
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.title("ROC curve")
+    for conf, tpr, fpr in conf_and_rates:
+        plt.annotate(f"{conf:.2f}", (fpr, tpr))
+
+    auc = area_under_curve(conf_and_rates[:, 1], conf_and_rates[:, 2])
+
+    plt.annotate(f"AUC: {auc:.2f}", (0.5, 0.5))
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    roc_curve_by_iou()
